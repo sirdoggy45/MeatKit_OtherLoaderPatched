a63402220be184993c403ecc224fa455e23fe191
Sirdoggy <sirdoggy45@gmail.com>
2026-04-06 22:10:48 +0200

    drop down for selecting ammo category in ammo page

--- Assets/Packages/Otherloader/Editor/SpawnerEntryEditor.cs
+++ Assets/Packages/Otherloader/Editor/SpawnerEntryEditor.cs
@@ -1,40 +1,64 @@
-﻿
-#if H3VR_IMPORTED
+﻿#if H3VR_IMPORTED
 using FistVR;
 using OtherLoader;
 #endif
 
-using System.Collections.Generic;
+using System;
 using System.Linq;
+using JetBrains.Annotations;
 using UnityEditor;
 using UnityEngine;
 
-[CustomEditor(typeof(OtherLoader.ItemSpawnerEntry), true)]
+[CustomEditor(typeof(ItemSpawnerEntry), true)]
 public class SpawnerEntryEditor : Editor
 {
-    public bool hasInit = false;
-    public bool isItemIDEmpty = false;
-
 #if H3VR_IMPORTED
-
+    
+    private enum AmmoCategoryType
+    {
+        None, Magazine, Clip, Speedloader, Cartridge
+    }
+    
+    private bool _hasInit;
+    private bool _isItemIDEmpty;
+    private ItemSpawnerV2.PageMode? _previousPage;
+    private AmmoCategoryType _ammoCategory = AmmoCategoryType.None;
+    
     public override void OnInspectorGUI()
     {
         serializedObject.ApplyModifiedProperties();
 
-        ItemSpawnerEntry entry = serializedObject.targetObject as ItemSpawnerEntry;
+        var entry = serializedObject.targetObject as ItemSpawnerEntry;
 
-        if (!hasInit)
+        if (!_hasInit)
         {
-            isItemIDEmpty = string.IsNullOrEmpty(entry.MainObjectID);
-            hasInit = true;
+            _isItemIDEmpty = string.IsNullOrEmpty(entry.MainObjectID);
+            
+            try
+            {
+                var parts = entry.EntryPath.Split('/');
+                _ammoCategory = (AmmoCategoryType)Enum.Parse(typeof(AmmoCategoryType), parts[1]);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            _hasInit = true;
         }
 
         var property = serializedObject.GetIterator();
-        if (!property.NextVisible(true)) return;
+        if (property == null || !property.NextVisible(true))
+            return;
+        
+        var page = (ItemSpawnerV2.PageMode)serializedObject.FindProperty("Page").enumValueIndex;
+        var subCatProperty = serializedObject.FindProperty("SubCategory");
+        var subCat = (ItemSpawnerID.ESubCategory)subCatProperty.enumValueIndex;
+        var isAmmoPage = page == ItemSpawnerV2.PageMode.Ammo;
+        var hasPageChanged = _previousPage != null && _previousPage != page;
 
         do
         {
-
             if (property.name == "MainObjectID")
             {
                 if (entry.MainObjectObj != null)
@@ -48,36 +72,68 @@ public class SpawnerEntryEditor : Editor
                 DrawHorizontalLine();
             }
 
+            if (property.name == "SubCategory")
+            {
+                if (isAmmoPage)
+                {
+                    _ammoCategory = (AmmoCategoryType)EditorGUILayout.EnumPopup(
+                        "Ammo Category",
+                        _ammoCategory
+                    );
+                }
+                else
+                    DrawProperty(property);
+
+                continue;
+            }
+
             if (property.name == "EntryPath")
             {
                 DrawHorizontalLine();
-
-                List<string> values = property.stringValue.Split('/').ToList();
-                property.stringValue = ((ItemSpawnerV2.PageMode)serializedObject.FindProperty("Page").enumValueIndex).ToString();
+                
+                var values = property.stringValue.Split('/').ToList();
+                property.stringValue = page.ToString();
                 property.stringValue += "/";
 
-                if (((ItemSpawnerID.ESubCategory)serializedObject.FindProperty("SubCategory").enumValueIndex) != ItemSpawnerID.ESubCategory.None)
+                // Ammo page doesn't use subcategories
+                if (isAmmoPage)
                 {
-                    property.stringValue += ((ItemSpawnerID.ESubCategory)serializedObject.FindProperty("SubCategory").enumValueIndex).ToString();
+                    if (hasPageChanged)
+                        _ammoCategory = AmmoCategoryType.None;
+                    
+                    subCatProperty.enumValueIndex = (int)ItemSpawnerID.ESubCategory.None;
+
+                    if (_ammoCategory != AmmoCategoryType.None)
+                        property.stringValue += _ammoCategory.ToString();
+                    else if (!hasPageChanged)
+                        property.stringValue += values[1];
                 }
                 else
                 {
-                    property.stringValue += values[1];
+                    if (hasPageChanged)
+                        subCatProperty.enumValueIndex = (int)ItemSpawnerID.ESubCategory.None;
+                    
+                    _ammoCategory = AmmoCategoryType.None;
+                    
+                    if (subCat != ItemSpawnerID.ESubCategory.None)
+                        property.stringValue += subCat.ToString();
+                    else if (!hasPageChanged)
+                        property.stringValue += values[1];
                 }
 
-                //Finally, add the end of the path based on the objectID
-                string itemID = serializedObject.FindProperty("MainObjectID").stringValue;
+                // Add ObjectID at the end of the path
+                var itemID = serializedObject.FindProperty("MainObjectID").stringValue;
                 if (!string.IsNullOrEmpty(itemID))
                 {
                     //If the itemID field is currently filled, but previously wasn't, we fill maintain all of the path and then add the itemID
-                    if (isItemIDEmpty)
+                    if (_isItemIDEmpty)
                     {
                         for (int i = 2; i < values.Count; i++)
                         {
                             property.stringValue += "/" + values[i];
                         }
 
-                        isItemIDEmpty = false;
+                        _isItemIDEmpty = false;
                     }
 
 
@@ -95,18 +151,21 @@ public class SpawnerEntryEditor : Editor
 
                 else
                 {
-                    isItemIDEmpty = true;
+                    _isItemIDEmpty = true;
 
                     for (int i = 2; i < values.Count; i++)
                     {
                         property.stringValue += "/" + values[i];
                     }
                 }
+                
             }
 
             DrawProperty(property);
         }
         while (property.NextVisible(false));
+
+        _previousPage = page;
     }
 
 
